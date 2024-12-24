469fb099098738eecaade5edc0d013e39769b1fb
Rameesa Jahan <rameesajahan22@gmail.com>
2024-12-24 22:58:33 +0530

    build issue fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 'use client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
--- components/columns.tsx
+++ components/columns.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { ColumnDef } from "@tanstack/react-table";
 
 export const columns: ColumnDef<any>[] = [
